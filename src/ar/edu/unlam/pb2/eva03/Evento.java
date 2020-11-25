841af9d1c8e19db5c3e3bff8bf6a6b53d92684fd
gasToledo <74069233+gasToledo@users.noreply.github.com>
2020-11-25 10:08:50 -0300

    Add files via upload

--- src/ar/edu/unlam/pb2/eva03/Evento.java
+++ src/ar/edu/unlam/pb2/eva03/Evento.java
@@ -2,6 +2,7 @@ package ar.edu.unlam.pb2.eva03;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -12,5 +13,38 @@ public class Evento {
 	private TipoDeEvento tipo;
 	private Integer numeroDeInscripcion;
 	private Map<Integer, Deportista> participantes;
+	
+	public Evento(TipoDeEvento tipo, Integer numeroDeInscripcion) {
+
+		this.tipo = tipo;
+		this.numeroDeInscripcion = numeroDeInscripcion;
+		participantes =  new HashMap<Integer, Deportista>();
+	}
+
+	public TipoDeEvento getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoDeEvento tipo) {
+		this.tipo = tipo;
+	}
+
+	public Integer getNumeroDeInscripcion() {
+		return numeroDeInscripcion;
+	}
+
+	public void setNumeroDeInscripcion(Integer numeroDeInscripcion) {
+		this.numeroDeInscripcion = numeroDeInscripcion;
+	}
+
+	public Map<Integer, Deportista> getParticipantes() {
+		return participantes;
+	}
+
+	public void setParticipantes(Map<Integer, Deportista> participantes) {
+		this.participantes = participantes;
+	}
+	
+	
 	
 }
